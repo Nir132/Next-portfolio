815f730222171c6ca9696e58542b3facf1aacc63
Nir132 <86047742+Nir132@users.noreply.github.com>
2024-04-04 00:33:37 +0300

    finished intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import {HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
@@ -43,10 +45,42 @@ export default function Intro() {
             building <span className="italic">sites & apps</span>. My focus is{" "}
             <span className="underline">Data and React (Next.js)</span>.
         </motion.h1>
-        <div>
-          <Link href="#contact" className='bg-gray-900 px-7 py-3 flex items-center gap-2 rounded-full text-cyan-100'>Contact me 
-           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>  
-        </div>
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center
+         gap-3 px-2 text-lg font-medium'
+         initial={{opacity:0, y:100}}
+         animate={{opacity:1 ,y:0}}
+         transition={{
+            delay:0.1
+         }}>
+          <Link href="#contact" className='bg-gray-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none
+          focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition text-cyan-100'>Contact me 
+           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+           </Link>
+           <a
+            className="group bg-white px-7 py-3 flex items-center gap-2  text-cyan-900 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-Black dark:bg-white/10"
+            href="/Nir Leibler Resume 2024.pdf"
+            download
+         >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-cyan-600 hover:text-cyan-850 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/nir-leibler-a937b91a9/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-cyan-600 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-cyan-850 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/Nir132"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+        </motion.div>
     </section>
   )
 }
