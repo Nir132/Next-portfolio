9354b1127a1208ff613271a57e7fbad6961c6486
Nir132 <86047742+Nir132@users.noreply.github.com>
2024-04-04 14:31:13 +0300

    some work

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import About from "@/components/About";
 import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
 import SectionDivider from "@/components/Section-divider";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Intro/>
       <SectionDivider/>
       <About/>
+      <Projects/>
     </main>
   )
 }
---
+++ components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from './Section-heading'
+
+export default function Projects() {
+  return (
+        <section>
+            <SectionHeading>
+                My Projects
+            </SectionHeading>
+        </section>
+  )
+}
