699dac096e47b4932f726170e32299255b92cd00
Nir132 <86047742+Nir132@users.noreply.github.com>
2024-04-07 15:50:10 +0300

    finnnished the custom hook for the sctions

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,27 +1,17 @@
 "use client";
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import {HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from 'react-icons/fa';
-import { useActiveSectionContext } from '@/context/active-section-context';
-import { useInView } from 'react-intersection-observer';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Intro() {
 
 
-  const {ref, inView} = useInView({
-      threshold:0.5
-  });
-  const {setActiveSection ,timeOfLastClick} = useActiveSectionContext()
-
-  useEffect(()=>{
-    if (inView && Date.now()- timeOfLastClick<1000){
-      setActiveSection("Home");
-    }
-  },[inView,setActiveSection, timeOfLastClick])
+ const {ref} = useSectionInView("Home" , 0.5)
 
   return (
     <section id='home' ref={ref} className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
