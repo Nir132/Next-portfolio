fd40354412ce4036ce1a5aba89db03f839a3cabf
Nir132 <86047742+Nir132@users.noreply.github.com>
2024-04-07 20:36:55 +0300

    start skills

---
+++ components/Skills.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from './Section-heading'
+
+export default function Skills() {
+  return (
+    <section>
+        <SectionHeading>
+            My skills
+        </SectionHeading>
+    </section>
+  )
+}
